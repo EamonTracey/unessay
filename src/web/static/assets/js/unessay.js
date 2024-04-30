0c6bd8d182dddec7b4f5a9e44174321af1d28531
eamontracey <etracey@nd.edu>
2024-04-30 06:25:39 -0400

    website lowkey looking nice now

--- src/web/static/assets/js/unessay.js
+++ src/web/static/assets/js/unessay.js
@@ -16,22 +16,40 @@ function fetchRecommendations(keywords) {
     .then(data => displayRecommendations(data));
 }
 
+// Function to display recommendations on the page
 function displayRecommendations(data) {
     var recommendationsContainer = document.getElementById('recommendations');
-    recommendationsContainer.innerHTML = '';
+    recommendationsContainer.innerHTML = ''; // Clear previous recommendations
 
     if (data && data.length > 0) {
         data.forEach(recipe => {
             var recipeElement = document.createElement('div');
-            recipeElement.innerHTML = `
-                <h3>${recipe.name}</h3>
-                <img src="${recipe.image}" alt="${recipe.name}">
-                <a href="${recipe.link}" target="_blank">View Recipe</a>
-            `;
+            recipeElement.classList.add('recipe'); // Add 'recipe' class
+
+            // Create link around the image
+            var link = document.createElement('a');
+            link.href = recipe.link;
+            link.target = '_blank'; // Open link in new tab
+            // Create image element
+            var image = document.createElement('img');
+            image.src = recipe.image;
+            image.alt = recipe.name;
+            // Append image to link
+            link.appendChild(image);
+            // Append link to recipe element
+            recipeElement.appendChild(link);
+
+            // Create heading for recipe name
+            var heading = document.createElement('h3');
+            heading.textContent = recipe.name;
+            // Append heading to recipe element
+            recipeElement.appendChild(heading);
+
+            // Append recipe element to recommendations container
             recommendationsContainer.appendChild(recipeElement);
         });
     } else {
-        recommendationsContainer.innerHTML = '<p>No recommendations found. Please try different words.</p>';
+        recommendationsContainer.innerHTML = '<p>No recipes found. Please try different ingredients.</p>';
     }
 }
 
